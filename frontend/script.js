866edb855378a0c3e690926b84049b6874887b5f
SHC-2k25 <shcommunicator123@gmail.com>
2025-10-18 23:00:41 -0300

    vamu vamu 2

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 // Produção (site HTTPS): "wss://SEU_BACKEND/?from=site"
 // Dev local:             "ws://localhost:8080/?from=site"
-const ENDERECO_WS = "wss://localhost:8080/?from=site"
+const ENDERECO_WS = "wss://app-shc-back.vercel.app/?from=site"
 
 let conexaoWs
 
